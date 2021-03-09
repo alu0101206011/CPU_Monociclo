fc5a60f25ff70289dbd388b8467bca18ea89bd37
Anabel Díaz Labrador <49716151+Sofant@users.noreply.github.com>
2021-03-09 21:41:10 +0000

    Primera versión sin probar que compila

--- cpu.v
+++ cpu.v
@@ -1,5 +1,10 @@
 module cpu(input wire clk, reset);
-//Procesador sin memoria de datos de un solo ciclo
+wire [5:0] opcode;
+wire [2:0] op_alu;
+wire s_inc, s_inm, we3, wez, z;
+
+cd camino_datos(clk, reset, s_inc, s_inm, we3, wez, op_alu, z, opcode);
+uc unidad_control(opcode, z, s_inc, s_inm, we3, wez, op_alu);
 
 
 endmodule
--- uc.v
+++ uc.v
@@ -2,79 +2,79 @@ module uc(input wire [5:0] opcode, input wire z, output reg s_inc, s_inm, we3, w
   always @(opcode)
   begin
     casex (opcode)
-      6'b1000xx:  
+      6'b1000xx:  // Alu (a)
         begin
           op_alu = 3'b000; 
-          s_inc = 1'b1;  // para que aumente pc
+          s_inc = 1'b1;  // para que aumente pc cuando no son saltos
           s_inm = 1'b0;  
           we3 = 1'b0;
-          wez = 1'b0;
+          wez = 1'b1;
         end
-      6'b1001xx:  
+      6'b1001xx:  // Alu (~a)
         begin
           op_alu = 3'b001; 
           s_inc = 1'b1;
           s_inm = 1'b0;
           we3 = 1'b0;
-          wez = 1'b0;
+          wez = 1'b1;
         end
-      6'b1010xx:  
+      6'b1010xx:  // Alu (a + b)
         begin
           op_alu = 3'b010; 
           s_inc = 1'b1;
           s_inm = 1'b0;
           we3 = 1'b0;
-          wez = 1'b0;
+          wez = 1'b1;
         end
-      6'b1011xx: 
+      6'b1011xx:  // Alu (a - b)
         begin 
           op_alu = 3'b011; 
           s_inc = 1'b1;
           s_inm = 1'b0;
           we3 = 1'b0;
-          wez = 1'b0;
+          wez = 1'b1;
         end
-      6'b1100xx:  
+      6'b1100xx:  // Alu (a & b)
         begin
           op_alu = 3'b100;
           s_inc = 1'b1;
           s_inm = 1'b0;
           we3 = 1'b0;
-          wez = 1'b0;
+          wez = 1'b1;
         end
-      6'b1101xx:  
+      6'b1101xx:  // Alu (a | b)
         begin
           op_alu = 3'b101; 
           s_inc = 1'b1;
           s_inm = 1'b0;
           we3 = 1'b0;
-          wez = 1'b0;
+          wez = 1'b1;
         end
-      6'b1110xx:  
+      6'b1110xx:  // Alu (-a)
         begin
           op_alu = 3'b110; 
           s_inc = 1'b1;
           s_inm = 1'b0;
           we3 = 1'b0;
-          wez = 1'b0;
+          wez = 1'b1;
         end
-      6'b1111xx:  
+      6'b1111xx:  // Alu (-b)
         begin    
           op_alu = 3'b111; 
           s_inc = 1'b1;
           s_inm = 1'b0;
           we3 = 1'b0;
-          wez = 1'b0;
+          wez = 1'b1;
         end
-      6'b0000xx:  
+      6'b0000xx:  // Carga inm
         begin
           op_alu = 3'b000; 
           s_inc = 1'b1;
-          s_inm = 1'b0;
-          we3 = 1'b0;
+          s_inm = 1'b1;
+          we3 = 1'b1;
           wez = 1'b0;
         end
-      6'b000100:  
+      6'b000100:  // J
         begin
           op_alu = 3'b000; 
           s_inc = 1'b0; 
@@ -82,18 +82,18 @@ module uc(input wire [5:0] opcode, input wire z, output reg s_inc, s_inm, we3, w
           we3 = 1'b0;
           wez = 1'b0;
         end
-      6'b000101:  
+      6'b000101:  // JZ
         begin
           op_alu = 3'b000; 
-          s_inc = 1'b0;
+          s_inc = z ? 1'b0 : 1'b1;
           s_inm = 1'b0;
           we3 = 1'b0;
           wez = 1'b0;
         end
-      6'b000110:  
+      6'b000110:  // JNZ
         begin
           op_alu = 3'b000; 
-          s_inc = 1'b0;
+          s_inc = z ? 1'b1 : 1'b0;
           s_inm = 1'b0;
           we3 = 1'b0;
           wez = 1'b0;
