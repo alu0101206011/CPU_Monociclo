631e66d07de34bb11448da766cf569bbb0bb481f
alu0101206011 <alu0101206011@ull.edu.es>
2022-03-08 16:31:16 +0000

    Señal overflow y carry añadidas al alu y camino de datos

--- alu.v
+++ alu.v
@@ -1,30 +1,37 @@
-module alu(input wire [15:0] a, b,
+module alu #(parameter WIDTH = 15) (input wire [WIDTH:0] a, b,
            input wire [2:0] op_alu,
-           output wire [15:0] y,
-           output wire zero);
+           output wire [WIDTH:0] y,
+           output wire carry, overflow, zero);
 
-reg [15:0] s;		   
-		   
-always @(a, b, op_alu)
-begin
-  case (op_alu)              
-    3'b000: s = a;
-    3'b001: s = ~a;
-    3'b010: s = a + b;
-    3'b011: s = a - b;
-    3'b100: s = a & b;
-    3'b101: s = a | b;
-    3'b110: s = -a;
-    3'b111: s = -b;
-	default: s = 'bx; //desconocido en cualquier otro caso (x � z), por si se modifica el c�digo
-  endcase
-end
+  reg [WIDTH + 1:0] s; // un bit mas de carry
+         
+  always @(a, b, op_alu)
+  begin
+    case (op_alu)              
+      3'b000: s = a;
+      3'b001: s = ~a;
+      3'b010: s = a + b;
+      3'b011: s = a - b;
+      3'b100: s = a & b;
+      3'b101: s = a | b;
+      3'b110: s = -a;
+      3'b111: s = -b;
+    default: s = 'bx; //desconocido en cualquier otro caso (x � z), por si se modifica el c�digo
+    endcase
+  end
 
-assign y = s;
+  parameter min = -(2 ** WIDTH);
+  parameter max = (2 ** WIDTH) - 1;
 
-//Calculo del flag de cero
-assign zero = ~(|y);   //operador de reducci�n |y hace la or de los bits del vector 'y' y devuelve 1 bit resultado
-		   
+  assign y = s[WIDTH:0];
+
+  assign overflow = (s < min | s > max) ? 1'b1 : 1'b0;
+  assign carry = s[WIDTH + 1];
+
+
+  //Calculo del flag de cero
+  assign zero = ~(|y);   //operador de reducci�n |y hace la or de los bits del vector 'y' y devuelve 1 bit resultado
+
 endmodule
 
 // Estaría bien poner más señales que solo cuando hay un 0. Estaría interesante poner cuando hay overflow, cuando hay acarreo.
--- pila.v
+++ pila.v
@@ -10,26 +10,26 @@ module pila(input wire clk, reset, push, pop, input wire[9:0] pc_addr, output re
       if (position[15:0] ==  15'b111111111111111)
         position[16] = 1'b1;
       else
-        position = position + 16'b0000000000000001;
+        position = position + 16'b1;
       mempila[position] <= pc_addr;
     end
     else if (pop)
     begin
-      mempila[position] = 10'b0000000000;
-      if (position[15:0] ==  15'b000000000000000)
+      mempila[position] = 10'b0;
+      if (position[15:0] ==  15'b0)
         position[16] = 1'b1;
       else
-        position = position - 16'b0000000000000001;
+        position = position - 16'b1;
     end
     else if (reset)
     begin
-      position = 16'b0000000000000000;
-      mempila[position] = 10'b0000000000;
+      position = 16'b0;
+      mempila[position] = 10'b0;
     end
     sp = mempila[position];
   end
 
-  assign underflow = reset | !((position[16] == 1) & (position[15:0] == 15'b000000000000000)) ? 1'b0 : 1'b1;
-  assign overflow = reset | !((position[16] == 1) & (position[15:0] == 15'b111111111111111)) ? 1'b0 : 1'b1;
+  assign underflow = reset | !((position[16] == 1'b1) & (position[15:0] == 15'b0)) ? 1'b0 : 1'b1;
+  assign overflow = reset | !((position[16] == 1'b1) & (position[15:0] == 15'b111111111111111)) ? 1'b0 : 1'b1;
 
 endmodule
